b7cb9aa1eb0e67d42a7b786137cf75865ec88ce6
lcanter65 <lisabrewercanter@gmail.com>
2026-04-07 16:17:16 -0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "lisAIconsulting";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Lisa AI Consulting';
+export const SITE_DESCRIPTION = 'AI Privacy, Governance, and Secure Adoption for Enterprise, Healthcare, and Government';
